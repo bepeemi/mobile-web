74ab8c76e93a516166e054b22f208c277b1aaa9c
bepeemi <bruno.pedroso11@gmail.com>
2025-09-01 16:21:10 -0300

    Realizando exercício JS

---
+++ javaScript/exer1.js
@@ -0,0 +1,14 @@
+let nome = "Bruno Pedroso";
+let letras = nome[0] + nome[6] ;
+
+/*
+JavaScript
+js
+*/
+
+let linguagem = "JavaScript";
+let inicial = linguagem[0] + linguagem[4];
+
+
+console.log(letras);
+console.log(inicial);
