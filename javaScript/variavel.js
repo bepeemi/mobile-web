381cce046e21c1aed6a9314fed4a473b40d938bb
bepeemi <bruno.pedroso11@gmail.com>
2025-09-01 15:53:19 -0300

    Aprendendo variáveis

---
+++ javaScript/variavel.js
@@ -0,0 +1,7 @@
+let mensagem = "Olá, mundo!";
+const nomeCompleto = "Bruno Pedroso";
+let idade = 26;
+// Resultado
+console.log(mensagem);
+console.log(nomeCompleto);
+console.log("Meu nome é: " + nomeCompleto + ", eu tenho: " + idade + " anos ");
