691eda5ab709429837376df6e3f1d2799d6a3efd
bepeemi <bruno.pedroso11@gmail.com>
2025-09-01 16:05:48 -0300

    Adicionando tipos de dados do JavaScript

---
+++ javaScript/tipos-dados.js
@@ -0,0 +1,19 @@
+//string
+let nome = "Bruno";
+
+//inteiro
+let idade = 26;
+
+//booleano
+let casado = false;
+
+//números flutuantes com casas decimais
+let valorProduto = 5.35;
+
+//resultados
+console.log("Meu nome é: " + nome);
+console.log("Minha idade é: ", idade);
+console.log("Casado? ", casado);
+console.log("R$", valorProduto);
+console.log(typeof idade); // ***TYPEOF*** serve para identificação de qual é a variável
+console.log(typeof nome);
