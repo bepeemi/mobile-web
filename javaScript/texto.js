de3af16e1d58f5aff575f86618f506f90ccb0086
bepeemi <bruno.pedroso11@gmail.com>
2025-09-02 14:26:37 -0300

    Adicionando Funções nativas do JS

---
+++ javaScript/texto.js
@@ -0,0 +1,17 @@
+let text = "Estão chegando as provas";
+
+//Tamanho da string
+console.log(text.length);
+
+//Replace
+console.log(text.replace("provas" , "avaliações"));
+
+//Colocar texto em maiusculo ou minusculo
+// EX: console.log(text.toUpperCase());
+ console.log(text.toLowerCase()); 
+
+//Retorna a primeira posição na string
+console.log(text.indexOf("as"));
+
+//Retorna a ultima posição na string
+console.log(text.lastIndexOf("as"));
