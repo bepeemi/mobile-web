902e799dcd1ad4228d76775c12b833789c5ab31c
bepeemi <bruno.pedroso11@gmail.com>
2025-09-02 16:19:47 -0300

    Adicionando os Botões das Operações numéricas na página

--- javaScript/calc.js
+++ javaScript/calc.js
@@ -1,8 +1,8 @@
 
 
 // Duas Variaveis
-let num1 = Number (prompt("digite um número"));
-let num2 = Number (prompt("digite outro número"));
+let num1 = 10;
+let num2 = 10;
 
 //Operações
 let soma = num1 + num2;
@@ -10,8 +10,14 @@ let sub = num1 - num2;
 let mult = num1 * num2;
 let div = num1 / num2;
 
-//Imprimir as respostas
-console.log("Soma:",soma);
-console.log("Subtração:",sub);
-console.log("Multiplicação:",mult);
-console.log("Divisão:",div);
+function somar(){
+   console.log(soma);}
+
+function sub(){
+    console.log(sub);}
+
+function mult(){
+    console.log(mult);}
+
+function div(){
+    console.log(div);}
