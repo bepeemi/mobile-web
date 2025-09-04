6a6a99949e2d0204a1923783ac8ac2da1cd8cab3
bepeemi <bruno.pedroso11@gmail.com>
2025-09-04 15:04:50 -0300

    Iniciando a Calculadora

--- javaScript/calculadora.js
+++ javaScript/calculadora.js
@@ -1,3 +1,9 @@
 // variavel
 const num1 = document.getElementById("num1");
 const num2 = document.getElementById("num2");
+const resultado = document.getElementById("resultado")
+
+// função
+function soma(){
+console.log(num1);
+}
