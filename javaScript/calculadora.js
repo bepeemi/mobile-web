e833247fcdc9f93103a19a7f654ef5340f3d727c
bepeemi <bruno.pedroso11@gmail.com>
2025-09-04 15:36:42 -0300

    Debugando o form utilizando o event.preventDefault()

--- javaScript/calculadora.js
+++ javaScript/calculadora.js
@@ -4,6 +4,7 @@ const num2 = document.getElementById("num2");
 const resultado = document.getElementById("resultado")
 
 // função
-function soma(){
+function soma(event){
+event.preventDefault();
 console.log(num1);
 }
