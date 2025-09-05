5369b8b03f426be0f3e480bfbba1bda5acce035a
bepeemi <bruno.pedroso11@gmail.com>
2025-09-05 14:51:34 -0300

    Aprendendo estrutura de decisão com If e Else

---
+++ javaScript/js-exemplos/idade.js
@@ -0,0 +1,9 @@
+let idade = 17;
+
+if (idade >= 18){
+    console.log("Você pode ir para festa!");
+
+}
+else {
+    console.log("Você não pode ir para festa!");
+}
---
+++ javaScript/js-exemplos/nota.js
@@ -0,0 +1,7 @@
+let nota = 7;
+if (nota >= 6){
+    console.log("Aprovado!");
+}
+else{
+    console.log("Reprovado!");
+}
