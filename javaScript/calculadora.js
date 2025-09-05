c757e4ebeafbcc8bd09f1cf7910c18be674bb335
bepeemi <bruno.pedroso11@gmail.com>
2025-09-05 15:22:02 -0300

    Continuando o projeto da Calculadora em JS

--- javaScript/calculadora.js
+++ javaScript/calculadora.js
@@ -6,6 +6,11 @@ const resultado = document.getElementById("resultado")
 // função
 function soma(event){
 event.preventDefault();
+//Validação de Formulário
+if (num1.value == "" || num2.value ==""){
+    alert("Preencha o campo");
+    return false;
+}
 resultado.innerHTML = Number(num1.value) + Number(num2.value);
 num1.value = "";
 num2.value = "";
