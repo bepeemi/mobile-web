66120430a36915f8c03a0d7378d4a0efe9799d60
bepeemi <bruno.pedroso11@gmail.com>
2025-09-09 15:44:51 -0300

    Imprimindo de formas diferentes o mesmo resultado

--- javaScript/exer1.js
+++ javaScript/exer1.js
@@ -1,3 +1,5 @@
+// for (let x = 1; x <= 10; x++) {
+//console.log("Essa é a " + x + "ª que essa frase é exibida");
 for (let x = 1; x <= 10; x++) {
-    console.log("Essa é a " + x + " que essa frase é exibida");
+    console.log(`Essa é a ${x}ª que essa frase é exibida`);
 }
