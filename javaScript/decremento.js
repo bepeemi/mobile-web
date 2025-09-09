5dd511387150b2d37a806892ee15485acdda7bcc
bepeemi <bruno.pedroso11@gmail.com>
2025-09-09 14:38:49 -0300

    Aprendendo sobre incremento e decremento

---
+++ javaScript/decremento.js
@@ -0,0 +1,23 @@
+let valor = 7;
+
+valor = valor -1;
+
+console.log(valor);
+
+valor--;
+
+console.log(valor);
+
+valor -= 2;
+
+console.log(valor);
+
+/* Multiplicação */
+valor *= 2;
+
+console.log(valor);
+
+/* Divisão */
+valor /= 4;
+
+console.log(valor);
---
+++ javaScript/incremento.js
@@ -0,0 +1,11 @@
+let valor = 7;
+
+console.log(valor);
+
+valor = valor + 1;
+
+console.log(valor);
+
+valor++;
+valor++;
+console.log(valor)
