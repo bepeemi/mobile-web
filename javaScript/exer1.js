9b867ff70201bc8da8e688731c33e3e1860c9f77
bepeemi <bruno.pedroso11@gmail.com>
2025-09-10 14:17:47 -0300

    Adicionando à pasta js-exemplos

--- javaScript/exer1.js
+++
@@ -1,5 +0,0 @@
-// for (let x = 1; x <= 10; x++) {
-//console.log("Essa é a " + x + "ª que essa frase é exibida");
-for (let x = 1; x <= 10; x++) {
-    console.log(`Essa é a ${x}ª que essa frase é exibida`);
-}
--- javaScript/js-exemplos/exer1.js
+++ javaScript/js-exemplos/exer1.js
@@ -1,14 +1,5 @@
-let nome = "Bruno Pedroso";
-let letras = nome[0] + nome[6] ;
-
-/*
-JavaScript
-js
-*/
-
-let linguagem = "JavaScript";
-let inicial = linguagem[0] + linguagem[4];
-
-
-console.log(letras);
-console.log(inicial);
+// for (let x = 1; x <= 10; x++) {
+//console.log("Essa é a " + x + "ª que essa frase é exibida");
+for (let x = 1; x <= 10; x++) {
+    console.log(`Essa é a ${x}ª que essa frase é exibida`);
+}
