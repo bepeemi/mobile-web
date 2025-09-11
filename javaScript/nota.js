506229527b88d8fc62d73dde74683c1ce107dda4
bepeemi <bruno.pedroso11@gmail.com>
2025-09-11 14:44:00 -0300

    Adicionando Validação de formulário

--- javaScript/nota.js
+++ javaScript/nota.js
@@ -1,14 +1,21 @@
 // Variáveis
 const inputNota = document.getElementById("nota");
 const resultado = document.getElementById("resultado");
+const message = document.getElementById("message");
 // Função
 function notaClick(event) {
-    event.preventDefault();
-    // Lógica
-    if (inputNota.value >= 6) {
-        resultado.innerHTML = "Aprovado";
-    } else {
-        resultado.innerHTML = "Reprovado";
-    }
-    inputNota.value = "";
+event.preventDefault();
+// Validação de formulário
+if (inputNota.value == "") {
+message.style.display = "block";
+return false;
+}  
+message.style.display = "none";
+// Lógica
+if (inputNota.value >= 6) {
+resultado.innerHTML = "Aprovado";
+} else {
+resultado.innerHTML = "Reprovado";
+}
+inputNota.value = "";
 }
