a5427787cb071c204b63ebdd680b6ac928027c13
bepeemi <bruno.pedroso11@gmail.com>
2025-09-12 14:45:54 -0300

    Organizando pasta Festa

---
+++ javaScript/niver.js
@@ -0,0 +1,7 @@
+function niverData(){
+const hoje = new Date();
+const diaSemana = hoje.getDay(); /* const = um valor que não muda // let = um valor que pode mudar */
+let diasFaltando = 4 - diaSemana;
+
+}
+niverData();
