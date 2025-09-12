17e992b3006ad6d2223e0045ddf38f27aa125d2b
bepeemi <bruno.pedroso11@gmail.com>
2025-09-12 16:09:21 -0300

    Exercício utilizando lógica de contagem regressiva

--- javaScript/niver.js
+++ javaScript/niver.js
@@ -3,5 +3,9 @@ const hoje = new Date();
 const diaSemana = hoje.getDay(); /* const = um valor que não muda // let = um valor que pode mudar */
 let diasFaltando = 4 - diaSemana;
 
+if (diasFaltando < 0){
+    diasFaltando += 7; 
+}
+document.getElementById("dias").textContent = diasFaltando
 }
 niverData();
