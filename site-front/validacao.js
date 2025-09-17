2529e14e177cbb8d761c1c925b20b3cbdb00224d
bepeemi <bruno.pedroso11@gmail.com>
2025-09-17 15:04:14 -0300

    Adicionando js do banner

--- site-front/validacao.js
+++ site-front/validacao.js
@@ -1,24 +1,25 @@
 const form = document.getElementById("newForm");
+const messageDiv = document.getElementById("message")
 
 form.addEventListener("submit", function(event) {
     // Impede envio automático
     event.preventDefault();
 
     const email = document.querySelector("#email").value.trim();
-
+    messageDiv.textContent = "";
     // Condição de valição 
     if (email === "") {
-        alert("Por favor, insira um e-mail");
+        messageDiv.textContent = "Por favor, insira um e-mail";
         return false;
     }
 
     // Validação simples: tem @ e .
     if (!email.includes("@") || !email.includes(".")) {
-        alert("Email inválido. Tente novamente");
+        messageDiv.textContent = "Email inválido. Tente novamente.";
         return;
     }
 
-    alert("Email cadastro com sucesso! ✅");
+    messageDiv.textContent = "Email cadastrado com sucesso! ✅";
 
     // Limpa o campo
     form.reset(); 
