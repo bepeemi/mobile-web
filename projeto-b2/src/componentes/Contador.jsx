a08296af76710ad3a5c369850ed217c99fcd2dca
bepeemi <bruno.pedroso11@gmail.com>
2025-09-19 16:19:59 -0300

    Incrementando CSS no React

--- projeto-b2/src/componentes/Contador.jsx
+++ projeto-b2/src/componentes/Contador.jsx
@@ -1,20 +1,24 @@
-import { Fragment } from "react";
+import { useState } from "react";
+import "./Contador.css";
 const Contador = () => {
+// Inicializa o estado com valor 0
+const [count, setCount] = useState(0);
+
 const aumentar = () =>{
-    alert ("Olá, Mundo!");
-    }
+    setCount(count + 1);   
+}
 const diminuir = () =>{
-    alert ("Adeus, Mundo!");
+    setCount(count - 1);
     }
 return (
-<Fragment>
-    <h1>Contador</h1>
+<div className="container">
+    <h1>{count}</h1>
     <section>
     <h2>Resultado</h2>
     <button onClick={aumentar} style={{marginRight: '10px'}}>Aumentar</button>
     <button onClick={diminuir}>Diminuir</button>
     </section>
-</Fragment>
+</div>
 )
     
 }
