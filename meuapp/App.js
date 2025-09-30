5e3749ebe69c5b80b7003596f8b50a1ef93e0315
Bruno Pedroso <140664574+bepeemi@users.noreply.github.com>
2025-09-30 16:24:55 -0300

    Estilizando uma loja no Android Studio

--- meuapp/App.js
+++ meuapp/App.js
@@ -1,36 +1,15 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StyleSheet, Text } from "react-native";
+import Prime from "./src/Telas/Prime";
 
 export default function App() {
 
   return (
     <>
-
-      <View style={estilos.topo}>
-        <Text style={estilos.tituloHeader}>Delicia Pastéis</Text>
-      </View>
-
       <View>
-        <Text>Faça o seu pedido aqui!</Text>
+        <Prime />
       </View>
-
     </>
   );
 }
 
-const estilos = StyleSheet.create({
 
-  topo: {
-    width: "100%",
-    height: 150,
-    backgroundColor: "#920606ff",
-  },
-
-  tituloHeader: {
-    width: "100%",
-    color: "#ffaa00ff",
-    textAlign: "center",
-    paddingTop: 60,
-    fontSize: 30,
-  },
-
-});
--- meuapp/src/Telas/Prime.js
+++ meuapp/src/Telas/Prime.js
@@ -1,9 +1,48 @@
-import { View, StylesSheet, Text } from "react-native";
+import { View, StyleSheet, Text, Button } from "react-native";
 
 export default function Prime() {
-  return (
-    <View>
-        <Text>TAPORRA MENOR</Text>
-    </View>
-  )
+    return (
+        <>
+
+            <View style={estilos.topo}>
+                <Text style={estilos.tituloHeader}>Delicia Pastéis</Text>
+            </View>
+
+            <View style={estilos.linha} />
+            <Text style={estilos.lojaContent}>Faça o seu pedido aqui!</Text>
+
+
+        </>
+    )
 }
+
+const estilos = StyleSheet.create({
+
+    topo: {
+        width: "100%",
+        height: 150,
+        backgroundColor: "#FFD700",
+    },
+
+    tituloHeader: {
+        width: "100%",
+        color: "#ff0000ff",
+        textAlign: "center",
+        paddingTop: 60,
+        fontSize: 30,
+    },
+    
+    linha: {
+        height: 2,
+        backgroundColor: "#ff0000ff",
+        width: "100%",
+
+    },
+
+    lojaContent: {
+        textAlign: "center",
+        fontSize: 20,
+        color: "#ff0000ff",
+    },
+
+});
