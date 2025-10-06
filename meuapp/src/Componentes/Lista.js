aa61bbd7533802be5d90f9149c69587dada1cd47
Bruno Pedroso <140664574+bepeemi@users.noreply.github.com>
2025-10-06 14:32:32 -0300

    Criando Lista de Objetos (Refrigerantes)

--- meuapp/src/Componentes/Lista.js
+++ meuapp/src/Componentes/Lista.js
@@ -5,28 +5,52 @@ export default function Lista() {
 
         {
             id: 1,
-            nome: 'Catupiry',
-            idade: 10,
+            nome: 'Água',
+            valor: 5,
 
         },
         {
             id: 2,
-            nome: 'Branch',
-            idade: 65,
+            nome: 'Coca-cola Lata 350ml',
+            valor: 7,
         },
         {
             id: 3,
-            nome: 'Gina',
-            idade: 19,
+            nome: 'Suco Natural',
+            valor: 8,
         },
     ];
     return (
-        <>
+    <View style={estilos.container}>
+        <Text style={estilos.titulo}>Bebidas</Text>
 
-        <View>
-            <Text>Teste</Text>
-        </View>
-        
-        </>
-    )
+        {lista.map((lista) => (
+            <View style={estilos.card}>
+                <Text>{lista.nome}</Text>
+                <Text>{lista.valor}</Text>
+            </View>
+        ))}
+    </View>
+  )
 }
+
+const estilos = StyleSheet.create({ 
+    container: {
+        padding: 20,
+        marginTop: 40,
+    }, 
+    titulo: {
+        fontSize: 22,
+        fontWeight: 'bold',
+        marginBottom: 15,
+        textAlign: 'center'
+    },
+    card: {
+        backgroundColor: "#fff",
+        padding: 15,
+        borderRadius: 10,
+        borderColor: "#dedede",
+        borderWidth: 1,
+        marginBottom: 10
+    }
+})
