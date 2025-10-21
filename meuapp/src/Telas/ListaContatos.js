24873885ac09f38b3b1c6340364e825b3ea3bdb8
bepeemi <bruno.pedroso11@gmail.com>
2025-10-21 16:20:45 -0300

    Configurando exclusão de contatos da API

--- meuapp/src/Telas/ListaContatos.js
+++ meuapp/src/Telas/ListaContatos.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Text, View, StyleSheet, ScrollView } from 'react-native';
+import { Text, View, StyleSheet, ScrollView, Button, Alert } from 'react-native';
 import axios from 'axios';
 
 export default function ListaContatos() {
@@ -7,7 +7,8 @@ export default function ListaContatos() {
 
   const listaContatos = () => {
     axios
-      .get("http://10.0.2.2:3000/contatos")
+      .get('http://10.0.2.2:3000/contatos')
+
       .then((resposta) => {
         setContatos(resposta.data);
       })
@@ -16,24 +17,44 @@ export default function ListaContatos() {
       });
   };
 
+  const deleteContato = (id) => {
+    axios
+      .delete(`http://10.0.2.2:3000/contatos/${id}`)
+      .then(() => {
+        setContatos(contatos.filter((contato) => contato.id !== id));
+        Alert.alert("Sucesso", "Contato Excluído");
+      })
+      .catch((error) => {
+        console.log("Erro", error);
+        Alert.alert("Erro ao excluir");
+      });
+  }
+
   useEffect(() => {
     listaContatos();
   }, []);
 
   return (
-    <ScrollView style={style.container}>
-      <Text style={style.headFont}>Contatos</Text>
-      {contatos.length > 0 ? (
-        contatos.map((contato, index) => (
-          <View key={index} style={style.card}>
-            <Text style={style.textName}>{contato.nome}</Text>
-            <Text style={style.textNum}>{contato.telefone}</Text>
-          </View>
-        ))
-      ) : (
-        <Text style={style.errorText}>Nenhum contato encontrado.</Text>
-      )}
-    </ScrollView>
+    <View style={style.container}>
+      <Text style={style.title}>Lista de Contatos</Text>
+
+      <ScrollView contentContainerStyle={style.listContainer}>
+        {contatos.length > 0 ? (
+          contatos.map((contato, index) => (
+            <View key={index} style={style.card}>
+              <Text style={style.textName}>{contato.nome}</Text>
+              <Text style={style.textNum}>{contato.telefone}</Text>
+              <Button
+                title="Excluir"
+                onPress={() => deleteContato(contato.id)}
+              />
+            </View>
+          ))
+        ) : (
+          <Text style={style.emptyText}>Nenhum contato disponível</Text>
+        )}
+      </ScrollView>
+    </View>
   );
 }
 
@@ -42,6 +63,9 @@ const style = StyleSheet.create({
     flex: 1,
     padding: 20,
     backgroundColor: "#ffffffff",
+  },
+  listContainer: {
+    paddingBottom: 20,
   },
   headFont: {
     fontWeight: 'bold',
@@ -59,7 +83,7 @@ const style = StyleSheet.create({
     shadowOffset: { width: 0, height: 10 },
     shadowOpacity: 0.5,
     shadowRadius: 6,
-    elevation: 6, 
+    elevation: 6,
   },
   textName: {
     fontSize: 18,
