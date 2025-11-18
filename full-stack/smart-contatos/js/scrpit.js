868a38cc6d1c1783e60d98681b075c961c09ab40
Bruno Pedroso <140664574+bepeemi@users.noreply.github.com>
2025-11-18 16:17:52 -0300

    Add atualizações

--- full-stack/smart-contatos/js/scrpit.js
+++ full-stack/smart-contatos/js/scrpit.js
@@ -1,16 +1,27 @@
 const form = document.querySelector("form")
-const inputNome = document.getElementById("nome");
+const list = document.querySelector(".lista")
+const inputName = document.getElementById("nome");
 const inputEmail = document.getElementById("email");
 const inputTel = document.getElementById("telefone");
 
 form.addEventListener("submit", function (event) {
     event.preventDefault();
-if (inputNome.value == "" || inputEmail.value == "" || inputTel.value == "" ) {
-    message.style.display = "block";
-    return false;
+    if (inputName.value == "" || inputEmail.value == "" || inputTel.value == "") {
+        alert("Digite seus dados")
+        return false;
 
-}
-    console.log("Nome", inputNome.value);
-    console.log("Email", inputEmail.value);
-    console.log("Telefone", inputTel.value);
+    }
+    const li = document.createElement("li");
+    li.innerHTML = `
+    <span class="contato-nome">${inputName.value}</span>
+    <span class="contato-email">${inputEmail.value}</span>
+    <span class="contato-telefone">${inputTel.value}</span>
+    `;
+    console.log(li);
+
+    list.appendChild(li);
+
+    form.reset();
+    
+
 });
